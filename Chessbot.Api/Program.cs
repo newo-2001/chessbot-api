0371f71e359fd72b6eaddd821c4126b5f1f95954
newo-2001 <s1122238@student.hsleiden.nl>
2022-11-29 11:50:02 +0100

    Added docker and stockfish

--- Chessbot.Api/Program.cs
+++ Chessbot.Api/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Chessbot.Api;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+foreach (var section in config.AsEnumerable().ToList())
+{
+    Console.WriteLine(section);
+}
+
+var stockfishPath = config.GetSection("STOCKFISH_PATH").Value
+        ?? config.GetRequiredSection("Stockfish").Value;
+var stockfish = new StockFishEngine(stockfishPath);
+
